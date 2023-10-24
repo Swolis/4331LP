e93eddc2f9464800a2ce22be9a5116ab48396066
jjoslin0994 <jjoslin0994@gmail.com>
2023-10-24 13:33:03 -0400

    fixed structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
-import AppRouter from './routes/AppRouter';
+import AppRouter from './routes/AppRouter'
 
 // import components
 
