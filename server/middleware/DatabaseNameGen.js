61632b169ed89cba9e4ef4661959436f3c302836
jjoslin0994 <jjoslin0994@gmail.com>
2023-11-12 07:51:08 -0500

    changed namegen to uuid

--- server/middleware/DatabaseNameGen.js
+++ server/middleware/DatabaseNameGen.js
@@ -1,20 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DatabaseNameGen = void 0;
+var mongodb_1 = require("mongodb");
 var DatabaseNameGen = function (req, res, next) {
-    if (!(req.url === '/Admin-Registration' || req.method === 'Post')) {
-        next();
-        return;
+    if (req.url === '/Admin-Registration' || req.method === 'POST') {
+        var uniqueId = new mongodb_1.ObjectId();
+        req.body.databaseName = uniqueId.toHexString();
     }
-    var s = '!#%&()+,-0123456789;=?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{}~';
-    var result = '';
-    for (var i = 0; i < 37; i++) {
-        var randomIndex = Math.floor(Math.random() * s.length);
-        result += s.charAt(randomIndex);
-    }
-    console.log("generated name: ".concat(result));
-    req.body.databaseName = result;
-    console.log(result);
     next();
 };
 exports.DatabaseNameGen = DatabaseNameGen;
--- server/middleware/DatabaseNameGen.ts
+++ server/middleware/DatabaseNameGen.ts
@@ -1,26 +1,12 @@
 // DatabaseNameGen.ts
 import { Request, Response, NextFunction } from 'express';
-import { ConnectToClientListMiddleWare } from './ConnectToClientListMiddleware';
+import { v4 as uuidv4 } from 'uuid';
 
-export const DatabaseNameGen = (req: Request, res: Response, next: NextFunction) =>
-{
-    if(!(req.url === '/Admin-Registration' || req.method === 'Post')){
-        next();
-        return;
-    }
-    const s = '!#%&()+,-0123456789;=?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{}~';
-    let result = '';
-
-    for(let i = 0; i < 37; i++){
-        const randomIndex = Math.floor(Math.random() * s.length);
-        result += s.charAt(randomIndex);
+export const DatabaseNameGen = (req: Request, res: Response, next: NextFunction) => {
+    if (req.url === '/Admin-Registration' || req.method === 'POST') {
+        const uniqueId = uuidv4();
+        (req as any).body.databaseName = uniqueId;
     }
 
-    console.log(`generated name: ${result}`);
-
-    (req as any).body.databaseName = result;
-
-    console.log(result);
     next();
-
-}
+};
