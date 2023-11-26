56133e1969aecb53fac756b4cda2dd71d3b61d19
jjoslin0994 <jjoslin0994@gmail.com>
2023-11-26 15:56:54 -0500

    update recipe handle

--- client/src/handlers/inventory/createRecipeHandler.jsx
+++ client/src/handlers/inventory/createRecipeHandler.jsx
@@ -5,7 +5,7 @@ const baseURL = `${window.location.protocol}//${window.location.hostname}:5000`;
 
 const createRecipe = async (RecipeData) => {
     try{
-        const response = await axios.post(`${baseURL}/Recipe-Router/Create-Recipe`, RecipeData, {mode: 'cors'});
+        const response = await axios.post(`${baseURL}/Recipe-Router/Create-Recipe`, RecipeData, {withCredentials: true, mode: 'cors'});
         return response.data;
     }catch (error) {
         throw new Error('Failed to Create Recipe');
