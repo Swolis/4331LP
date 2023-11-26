4e65cc357eea7eee9994de7bab1e456912be0316
jjoslin0994 <jjoslin0994@gmail.com>
2023-11-26 00:32:13 -0500

    fix pin

--- client/src/components/login/changeDefaultPin.jsx
+++ client/src/components/login/changeDefaultPin.jsx
@@ -1,51 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const PinUpdateCard = ({ onSubmit }) => {
-  const [name, setName] = useState("");
-  const [currentPin, setCurrentPin] = useState("");
-  const [newPin, setNewPin] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (name && currentPin.length === 4 && newPin.length === 4) {
-      onSubmit(name, currentPin, newPin);
-    } else {
-      console.error(
-        "Please enter your name and both current and new 4-digit PINs."
-      );
-    }
+    const [name, setName] = useState("");
+    const [newPin, setNewPin] = useState("");
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+  
+      if (name && newPin.length === 4) {
+        onSubmit(name, newPin);
+      } else {
+        console.error("Please enter your name and a new 4-digit PIN.");
+      }
+    };
+  
+    return (
+      <div>
+        <h2>Update your PIN</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Your Name:
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+  
+          <br />
+          <label>
+            New PIN:
+            <input
+              type="password"
+              name="newPin"
+              value={newPin}
+              onChange={(e) => setNewPin(e.target.value)}
+            />
+          </label>
+          <br />
+          <button type="submit">Update Pin</button>
+        </form>
+      </div>
+    );
   };
-
-  return (
-    <div>
-      <h2>Update your PIN</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Your Name:
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-
-        <br />
-        <label>
-          New PIN:
-          <input
-            type="password"
-            name="newPin"
-            value={newPin}
-            onChange={(e) => setNewPin(e.target.value)}
-          />
-        </label>
-        <br />
-        <button type="submit">Update Pin</button>
-      </form>
-    </div>
-  );
-};
-
-export default PinUpdateCard;
+  
+  export default PinUpdateCard;
--- client/src/handlers/LoginHandler.js
+++ client/src/handlers/LoginHandler.js
@@ -34,46 +34,58 @@ const handleLogin = async (formState) => {
 async function handleSuccessfulLogin(loginData) {
     // Log all cookies
     console.log(`All Cookies: ${document.cookie}`);
-
+  
     // Check and log the authToken cookie
-    const authTokenCookie = document.cookie.split(';').map(cookie => cookie.trim()).find(cookie => cookie.startsWith('authToken='));
-
+    const authTokenCookie = document.cookie
+      .split(";")
+      .map((cookie) => cookie.trim())
+      .find((cookie) => cookie.startsWith("authToken="));
+  
     if (authTokenCookie) {
-        const authToken = authTokenCookie.split('=')[1]; // Extract the value of the authToken
-        const cookieObject = jwtDecode(authToken);
-
+      const authToken = authTokenCookie.split("=")[1]; // Extract the value of the authToken
+      const cookieObject = jwtDecode(authToken);
+  
+      useEffect(() => {
+        // Check if defaultPin is true and render PinUpdateCard accordingly
         if (cookieObject.defaultPin) {
-            // Prompt user to update PIN
-            try {
-                const { name, newPin } = PinUpdateCard(); // Assuming PinUpdateCard() returns the new PIN
-
-                if (newPin) {
-                    // Make an API call to update the PIN
-                    const response = await axios.post(`${baseURL}/Update-Pin`, {
-                        name,
-                        pin: newPin,
-                        permission: true,
-                        // Include any other necessary data in the request body
-                    });
-
-                    // Handle the response as needed
-                    console.log('PIN update response:', response.data);
-
-                    // If PIN is successfully updated, redirect to the dashboard
-                    window.location.href = '/clientDashboard';
-                }
-            } catch (error) {
-                console.error('Error updating PIN:', error.message);
-                // Handle the error as needed
-            }
-        } else {
-            // Set the authToken in the Axios headers for subsequent requests
-            axios.defaults.headers.common['Authorization'] = `Bearer ${authTokenCookie.split('=')[1]}`;
-            
-            // Redirect to the dashboard if the PIN is not the default
-            window.location.href = '/clientDashboard';
+          const { name, newPin } = PinUpdateCard();
+          // Render the PinUpdateCard component based on the condition
+          // You can also use this information to conditionally render other components or take actions
         }
+      }, []); // Empty dependency array ensures that the useEffect runs only once when the component mounts
+  
+      if (cookieObject.defaultPin) {
+        // Prompt user to update PIN
+        try {
+          const { name, newPin } = PinUpdateCard(); // Assuming PinUpdateCard() returns the new PIN
+  
+          if (newPin) {
+            // Make an API call to update the PIN
+            const response = await axios.post(`${baseURL}/Update-Pin`, {
+              name,
+              pin: newPin,
+              permission: true,
+              // Include any other necessary data in the request body
+            });
+  
+            // Handle the response as needed
+            console.log("PIN update response:", response.data);
+  
+            // If PIN is successfully updated, redirect to the dashboard
+            window.location.href = "/clientDashboard";
+          }
+        } catch (error) {
+          console.error("Error updating PIN:", error.message);
+          // Handle the error as needed
+        }
+      } else {
+        // Set the authToken in the Axios headers for subsequent requests
+        axios.defaults.headers.common["Authorization"] = `Bearer ${authTokenCookie.split("=")[1]}`;
+  
+        // Redirect to the dashboard if the PIN is not the default
+        window.location.href = "/clientDashboard";
+      }
     }
-}
-
-export default handleLogin;
+  }
+  
+  export default handleLogin;
