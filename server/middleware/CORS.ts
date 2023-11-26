06b84ec009174ba0ff15dc115d262c187da1a4cc
jjoslin0994 <jjoslin0994@gmail.com>
2023-11-25 19:35:39 -0500

    test if ts is configured correctly for the server

--- server/middleware/CORS.ts
+++ server/middleware/CORS.ts
@@ -3,8 +3,8 @@ import { CorsOptions } from 'cors';
 
 export const corsConfig: CorsOptions = {
 
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    origin: ['http://businesscraft.work', 'https://businesscraft.work'],
+    methods: ['GET', 'HEAD', 'POST', 'PUT','PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 };
