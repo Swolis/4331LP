ba212be59310afa1cc046f4b1767374c0e8df16e
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-11-27 13:44:06 -0500

    Delete server/middleware directory

--- server/middleware/CORS.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.corsConfig = void 0;
-exports.corsConfig = {
-    origin: ['http://businesscraft.work', 'https://businesscraft.work'],
-    methods: ['GET', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-};
