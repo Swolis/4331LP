4a4fc1dc204a2e89c1794791e37c8c4e584e4c65
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-11-27 13:44:52 -0500

    Delete middleware directory

--- middleware/CORS.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.corsConfig = void 0;
-exports.corsConfig = {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-};
