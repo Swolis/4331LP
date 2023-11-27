724370ba8c47e44a4bf0966fa455d69756d5a5de
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-11-27 13:47:48 -0500

    Delete server/middleware directory

--- server/middleware/loggingMiddleware.ts
+++
@@ -1,9 +0,0 @@
-// loggingMiddleware.js
-import {Request, Response, NextFunction } from 'express';
-export const loggingMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
-    console.log('Headers:', req.headers);
-    console.log('Body:', req.body);
-    next();
-  };
-    
